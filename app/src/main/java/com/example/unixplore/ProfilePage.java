f6298e1d99ebb4776565a08f555afe92571f8358
LINCIA-DSOUZA <linciads03@gmail.com>
2024-03-16 21:37:43 +0530

    Login Page

---
+++ app/src/main/java/com/example/unixplore/ProfilePage.java
@@ -0,0 +1,107 @@
+package com.example.unixplore;
+
+import static com.example.unixplore.R.id.progessBar;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+//import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class ProfilePage extends AppCompatActivity {
+
+    private TextView textViewWelcome, textViewFullName, textViewEmail, textViewDoB, textViewGender, textViewMobile;
+
+    private ProgressBar progressBar;
+
+    private String fullName, email, doB, gender, mobile;
+
+    private ImageView imageView;
+
+    private FirebaseAuth authProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile_page);
+
+        getSupportActionBar().setTitle("Profile");
+
+        textViewWelcome = findViewById(R.id.textView_show_welcome);
+        textViewFullName = findViewById(R.id.textView_show_full_name);
+        textViewEmail = findViewById(R.id.textView_show_email);
+        textViewDoB = findViewById(R.id.textView_show_dob);
+        textViewGender = findViewById(R.id.textView_show_gender);
+        textViewMobile = findViewById(R.id.textView_show_mobile);
+        progressBar = findViewById(R.id.progessBar);
+
+        authProfile = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = authProfile.getCurrentUser();
+
+        if (firebaseUser == null){
+            Toast.makeText(ProfilePage.this, "Something went wrong. User details are not available at the moment", Toast.LENGTH_LONG).show();
+        } else {
+            progressBar.setVisibility(View.VISIBLE);
+            showUserProfile(firebaseUser);
+        }
+
+
+
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+
+    private void showUserProfile(FirebaseUser firebaseUser) {
+        String userID = firebaseUser.getUid();
+
+        DatabaseReference referenceProfile = FirebaseDatabase.getInstance().getReference("Registered Users");
+        referenceProfile.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                ReadWriteUserDetails readWriteUserDetails = snapshot.getValue(ReadWriteUserDetails.class);
+                if (readWriteUserDetails != null){
+                    fullName = firebaseUser.getDisplayName();
+                    email = firebaseUser.getEmail();
+                    doB = readWriteUserDetails.doB;
+                    gender = readWriteUserDetails.gender;
+                    mobile = readWriteUserDetails.mobile;
+
+                    textViewWelcome.setText("Welcome, " + fullName + "!");
+                    textViewFullName.setText(fullName);
+                    textViewEmail.setText(email);
+                    textViewDoB.setText(doB);
+                    textViewGender.setText(gender);
+                    textViewMobile.setText(mobile);
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ProfilePage.this, "Something went wrong.", Toast.LENGTH_LONG).show();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+}
